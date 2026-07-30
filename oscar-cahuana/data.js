ec87d128a1ce054eee7d4013021cc1e52df08c04
endecorpcbba <endecorp.cbba@gmail.com>
2026-07-30 15:11:20 -0400

    Update data.js

--- oscar-cahuana/data.js
+++ oscar-cahuana/data.js
@@ -1,7 +1,7 @@
 const funcionario = {
     nombre: "Oscar Cahuana Alarcon",
     cargo: "JEFE DEPTO. EJECUCION PROYECTOS GENERACION",
-    unidad: "GERENCIA DE EJECUCION DE PROYECTOS - ENDE Corporación",
+    unidad: "GERENCIA DE EJECUCION DE PROYECTOS - ENDE",
 
     telefono: "+59172563747",
     whatsapp: "59172563747",
