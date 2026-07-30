273b1954aed4cced23bdb5832ff266fee2623d88
endecorpcbba <endecorp.cbba@gmail.com>
2026-07-30 14:16:47 -0400

    Oscar Cahuna Create

---
+++ oscar-cahuana/data.js
@@ -0,0 +1,9 @@
+const funcionario = {
+    nombre: "Gonzalo Sánchez",
+    cargo: "Técnico de Soporte a Usuarios",
+    unidad: "USTI - ENDE Corporación",
+
+    telefono: "+59176410429",
+    whatsapp: "59176410429",
+    correo: "gonzalo.sanchez@ende.bo"
+};
