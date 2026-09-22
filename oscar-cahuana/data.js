35afee6f219a4e79124bff1ed0f7e4637b7428da
endecorpcbba <endecorp.cbba@gmail.com>
2026-09-22 15:38:50 -0400

    .

--- oscar-cahuana/data.js
+++ oscar-cahuana/data.js
@@ -1,7 +1,7 @@
 const funcionario = {
     nombre: "Oscar Cahuana Alarcon",
-    cargo: "GERENTE EJECUCION PROYECTOS",
-    unidad: "GERENCIA EJECUCION DE PROYECTOS",
+    cargo: "GERENTE DE EJECUCION PROYECTOS",
+    unidad: "GERENCIA DE EJECUCION DE PROYECTOS",
 
     telefono: "+59172563747",
     whatsapp: "59172563747",
